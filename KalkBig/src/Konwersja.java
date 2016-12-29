21e8443c01b5b332fe41dc93cf9c190fa465bef8
DanusiaZabcia <ozesek@wp.pl>
2016-12-29 14:40:59 +0100

    new Integer.parseInt method

--- KalkBig/src/Konwersja.java
+++ KalkBig/src/Konwersja.java
@@ -8,15 +8,15 @@ public class Konwersja {
 		byte a= 65;
 		int c = 2;
 		double b = 3.5;
-		char znak = 'v';
 		double kon2 = c/b; //konwersja automatyczna
-		
-		
+		String napis = "10";
+		int wynik= 20 + Integer.parseInt(napis);
 		
 		char kon1 = (char)a; // byte to char
 		System.out.println(kon1); 
 		System.out.println((char) (kon1 + c)); // int na char
 		System.out.println(kon2);
+		System.out.println(wynik);
 		
 	}
 }
