c0bbb4309369da2269a0426ca2508dba04974a57
DanusiaZabcia <ozesek@wp.pl>
2016-12-30 12:24:12 +0100

    exception no such element

--- KalkBig/src/PobieranieDanych.java
+++ KalkBig/src/PobieranieDanych.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
- 
+import static java.lang.Math.*;
 
 public class PobieranieDanych {
 
@@ -12,7 +12,27 @@ public class PobieranieDanych {
 		      System.out.println("Witaj "+imie); //wyœwietlamy powitanie
 		     
 		      odczyt.close();
-		
-	}
-
+		/*  Utwórz dwie zmienne typu double. Nastêpnie przy u¿yciu klasy Scanner 
+		 * pobierz od u¿ytkownika dwie liczby i wykonaj na nich dodawanie, odejmowanie
+		 * , mno¿enie i dzielenie, wyœwietlaj¹c wyniki w kolejnych liniach na  konsoli.*/
+	
+		  double zmienna1= 234.23;
+		  double zmienna2= 32.34;
+		  double zmiennaA;
+		  double zmiennaB;
+		  Scanner odczytA= new Scanner(System.in);
+		  Scanner odczytB= new Scanner(System.in);
+		  System.out.println(imie+ " podaj pierwsz¹ liczbê dzia³ania." );
+		   zmiennaA = odczytA.nextDouble();
+		   
+		   System.out.println(imie+ " podaj drug¹ liczbê dzia³ania." );
+		   
+		   zmiennaB = odczytB.nextDouble();
+		   
+		   System.out.println(imie+ "to Twój wynik z dodawania= " + zmienna1+zmiennaA);
+		 
+		  
+		   odczytA.close();
+		   odczytB.close();
+}
 }
