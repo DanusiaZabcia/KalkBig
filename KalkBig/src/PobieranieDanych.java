2d0ea55e78b5a4a68512fb4cc23c289844b38ca1
DanusiaZabcia <ozesek@wp.pl>
2016-12-30 13:14:43 +0100

    the end

--- KalkBig/src/PobieranieDanych.java
+++ KalkBig/src/PobieranieDanych.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.*;
 import static java.lang.Math.*;
 
 public class PobieranieDanych {
@@ -15,24 +15,23 @@ public class PobieranieDanych {
 		/*  Utwórz dwie zmienne typu double. Nastêpnie przy u¿yciu klasy Scanner 
 		 * pobierz od u¿ytkownika dwie liczby i wykonaj na nich dodawanie, odejmowanie
 		 * , mno¿enie i dzielenie, wyœwietlaj¹c wyniki w kolejnych liniach na  konsoli.*/
-	
-		  double zmienna1= 234.23;
-		  double zmienna2= 32.34;
+		  double zmiennaB;
 		  double zmiennaA;
-		 
+		  odczyt.useLocale(Locale.ENGLISH); // receives dots instead of commas
 		 
 		  System.out.println(imie+ " podaj pierwsz¹ liczbê dzia³ania." );
 		   zmiennaA = odczyt.nextDouble();
 		  
-		   double zmiennaB;
+		   
 		   
 		   System.out.println(imie+ " podaj drug¹ liczbê dzia³ania." );
 		   
 		   zmiennaB = odczyt.nextDouble();
 		   
-		   System.out.println(imie+ " to Twój wynik z dodawania= " + zmienna1+zmiennaA);
-		 
-		  
+		   System.out.printf(imie+ " to Twój wynik z dodawania= " + "%.2f%n" ,(zmiennaA+zmiennaB));
+		   System.out.printf(imie+ " to Twój wynik z odejmnowania= " + "%.2f%n" ,(zmiennaA-zmiennaB));
+		   System.out.printf(imie+ " to Twój wynik z mno¿enia= " + "%.2f%n" ,(zmiennaA*zmiennaB));
+		   System.out.printf(imie+ " to Twój wynik z dzielenia= " + "%.2f%n" ,(zmiennaA/zmiennaB));
 		   odczyt.close();
 		  
 }
