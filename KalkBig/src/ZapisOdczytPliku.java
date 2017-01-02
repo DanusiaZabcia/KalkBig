c973055f203b81f801b2f329caa403ff4bc3bcf6
DanusiaZabcia <ozesek@wp.pl>
2017-01-02 14:22:56 +0100

    fixed writing

--- KalkBig/src/ZapisOdczytPliku.java
+++ KalkBig/src/ZapisOdczytPliku.java
@@ -1,11 +1,14 @@
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class ZapisOdczytPliku {
 
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws IOException {
 		
 		final File PLIK = new File("ala2.txt");
         final Scanner ODCZYT = new Scanner(PLIK);
@@ -15,9 +18,9 @@ public class ZapisOdczytPliku {
         System.out.println(ZDANIE);
         ODCZYT.close();
         
-        final PrintWriter ZAPIS = new PrintWriter("ala.txt");
+        final PrintWriter ZAPIS = new PrintWriter(new BufferedWriter(new FileWriter("ala.txt", true)));
         
-        ZAPIS.println("I zielone myszki te¿ ma");
+        ZAPIS.append("I zielone myszki te¿ ma");
         ZAPIS.close();
         
 	}
