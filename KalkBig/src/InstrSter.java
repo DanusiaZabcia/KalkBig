a1d328385962e2abd551ff0d006895443723ccac
DanusiaZabcia <ozesek@wp.pl>
2017-01-02 12:45:11 +0100

    Add comparing names

--- KalkBig/src/InstrSter.java
+++ KalkBig/src/InstrSter.java
@@ -1,33 +1,65 @@
 import java.util.*;
 
-
 public class InstrSter {
-/*1.9 Pobierz w konsoli dwie liczby ca³kowite,
- *  nastêpnie porównaj je i wyœwietl stosowny komunikat z wynikiem. 
- */
-	
-	public static void main ( String[] args){
-		
-		final Integer PIERWSZA ;
+	/*
+	 * 1.9 Pobierz w konsoli dwie liczby ca³kowite, nastêpnie porównaj je i
+	 * wyœwietl stosowny komunikat z wynikiem.
+	 */
+
+	public static void main(String[] args) {
+
+		final Integer PIERWSZA;
 		final Integer DRUGA;
-		final Scanner ODCZYT = new Scanner(System.in) ;
+		final Scanner ODCZYT = new Scanner(System.in);
 		System.out.println("Podaj pierwsz¹ liczbê ca³kowit¹");
-		
+
 		PIERWSZA = ODCZYT.nextInt();
-		
+
 		System.out.println("Podaj drug¹ liczbê ca³kowit¹");
 		DRUGA = ODCZYT.nextInt();
-		
-		if (PIERWSZA > DRUGA )
+
+		if (PIERWSZA > DRUGA)
 			System.out.println("Pierwsza liczba jest wiêksza od drugiej");
-			
+
 		else if (PIERWSZA < DRUGA)
 			System.out.println("Pierwsza liczba jest mniejsza od drugiej");
-			
+
 		else
 			System.out.println("Liczby s¹ takie same");
 		
-		ODCZYT.close();
 		
+		/*
+		 * Napisz program, który pobierze od u¿ytkownika imiê i przechowa je w
+		 * zmiennej. Nastêpnie stwórz kilka warunków z ró¿nymi imionami. Jeœli
+		 * któreœ z imion bêdzie pasowa³o wyœwietl "Czeœæ jakieœ_imiê", gdy
+		 * program nie znajdzie imienia wyœwietl "Przykro mi, ale Ciê nie znam".
+		 */
+
+		final String IMIE;
+		//Scanner ODCZYT = new Scanner(System.in);
+		System.out.println("Podaj swoje imiê ");
+		ODCZYT.nextLine();
+		IMIE = ODCZYT.nextLine();
+
+		switch (IMIE) {
+
+		case "Danusia":
+			System.out.println("Czeœæ " + IMIE);
+			break;
+		case "Kamil":
+			System.out.println("Czeœæ " + IMIE);
+			break;
+		case "Alicja":
+			System.out.println("Czeœæ " + IMIE);
+			break;
+		case "Anna":
+			System.out.println("Czeœæ " + IMIE);
+			break;
+		default:
+			System.out.println("Przykro mi, nie znam Ciê");
+		}
+
+		ODCZYT.close();
+
 	}
 }
