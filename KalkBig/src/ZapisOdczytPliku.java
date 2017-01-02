68b29ca6b770840de9c656a242365e92fced7d03
DanusiaZabcia <ozesek@wp.pl>
2017-01-02 14:09:45 +0100

    Writing and reading file.txt

--- KalkBig/src/ZapisOdczytPliku.java
+++ KalkBig/src/ZapisOdczytPliku.java
@@ -1,18 +1,25 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.io.PrintWriter;
 
 public class ZapisOdczytPliku {
 
 	public static void main(String[] args) throws FileNotFoundException {
 		
-		final File PLIK = new File("ala.txt");
+		final File PLIK = new File("ala2.txt");
         final Scanner ODCZYT = new Scanner(PLIK);
         
         final String ZDANIE = ODCZYT.nextLine();
         
         System.out.println(ZDANIE);
         ODCZYT.close();
+        
+        final PrintWriter ZAPIS = new PrintWriter("ala.txt");
+        
+        ZAPIS.println("I zielone myszki te¿ ma");
+        ZAPIS.close();
+        
 	}
 }
 
