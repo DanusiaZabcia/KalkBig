ce578ce19341cc7f7527b0d81fe7b30b1cf09706
DanusiaZabcia <ozesek@wp.pl>
2017-01-12 16:26:13 +0100

    two bombs explode

--- KalkBig/src/bomba.java
+++ KalkBig/src/bomba.java
@@ -7,16 +7,31 @@ public class bomba {
 		Scanner odczyt = new Scanner(System.in);
 		System.out.println("Podaj za ile sekund ma wybuchnac bomba");
 		Liczba = odczyt.nextInt();
-		odczyt.close();
-		int i = Liczba;
-		do { 
-			System.out.println("Bomba wybuchni za "+ i + "sekund" );
-			i--;
-		}while (i>0);
 		
+		
+		int i = Liczba;
+		do {
+			System.out.println("Bomba wybuchni za " + i + "sekund");
+			i--;
+		} while (i > 0);
+
 		System.out.println("Bum!");
+		odczyt.nextLine();
+		int Liczba_2;
+		
+		
+		
+		System.out.println("Podaj za ile sekund ma wybuchnac druga bomba");
+		Liczba_2= odczyt.nextInt();
+		
+		int j = Liczba_2;
+		odczyt.close();
+
+		for (int g = j; g > 0; g--) {
+			System.out.println("Druga bomba wybuchnie za " + j + "sekund");
+			j--;
 		}
+		System.out.println("Bum Bum!");
 
-	
-
+	}
 }
