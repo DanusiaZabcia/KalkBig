e7e01ffa692e78e430802243b1b292a30fe4db7d
DanusiaZabcia <ozesek@wp.pl>
2017-01-12 13:57:02 +0100

    while loop

---
+++ KalkBig/src/petle.java
@@ -0,0 +1,17 @@
+
+public class petle {
+
+	public static void main(String[] args) {
+		
+		int licznik= 0;
+		
+		 while ( licznik < 10)
+		 {
+			 System.out.println("To jest pêtla");
+			 licznik ++;
+			 
+		 }
+		 System.out.println("Koniec pêtli");
+	}
+
+}
