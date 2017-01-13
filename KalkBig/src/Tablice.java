b60374d0de21ae9de5238a8280c0568e40700737
DanusiaZabcia <ozesek@wp.pl>
2017-01-13 11:37:44 +0100

    arrays

---
+++ KalkBig/src/Tablice.java
@@ -0,0 +1,16 @@
+
+public class Tablice {
+
+	public static void main(String[] args) {
+		int liczby[] = new int[10];
+		
+		for (int i=0; i<10; i++){
+			liczby[i]= i+1;
+			
+			int zmienna= liczby[i];
+			System.out.println("kolejna komórka to "+ zmienna);
+		}
+
+	}
+
+}
