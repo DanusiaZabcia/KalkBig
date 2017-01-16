587ab72bbbd19aa26f4fed6afad61140fc45a56f
DanusiaZabcia <ozesek@wp.pl>
2017-01-16 12:02:30 +0100

    two loops: for and while

--- KalkBig/src/TablinaN.java
+++ KalkBig/src/TablinaN.java
@@ -3,32 +3,34 @@ import java.util.Scanner;
 public class TablinaN {
 
 	public static void main(String[] args) {
-		
+
 		Scanner odczyt = new Scanner(System.in);
-		 int n;
-		 
-		 int[] tablica;
-		 
-		 System.out.println("Podaj wielkoœæ talbicy");
-		 n= odczyt.nextInt();
-		 
-		 
-		 int j=1;
-		 odczyt.close();
-		 tablica= new int[n];
-		 
-		 while(n>0){ 
-			 n--;
-			 tablica[n]=j;
-			 j++;
-			 
-			 System.out.println( n+" element tablicy to "+ tablica[n]); 
-		 }
-		 
+		int n;
+
+		int[] tablica;
+
+		System.out.println("Podaj wielkoœæ talbicy");
+		n = odczyt.nextInt();
+
 		
-		  
-		 
-		 }
+		int h = n;
+		
+		odczyt.close();
+		tablica = new int[n];
+
+		
+		for (int i = 0; i < h; i++) {
+			
+			
+			tablica[i] = i;
+			
+		}
+		while (n > 0) {
+			n--;
+			System.out.println(n + " element tablicy to " + tablica[n]);
+			
+
+		}
+
 	}
-
-
+}
