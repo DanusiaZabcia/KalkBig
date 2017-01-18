6d975ae5d39aae1d698a34c60aae27a57322bc29
DanusiaZabcia <ozesek@wp.pl>
2017-01-18 15:43:24 +0100

    ok

--- KalkBig/src/Binarne_strumienie.java
+++ KalkBig/src/Binarne_strumienie.java
@@ -3,23 +3,43 @@ import java.io.DataOutputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.RandomAccessFile;
 
 public class Binarne_strumienie {
 
 	public static void main(String[] args) {
-		
+		String plik = "binarnie.txt";
+		int iloscSczytanychBajtow = 0;
+		RandomAccessFile strumien = null;
 		
 		try{
-		String plik = "binarnie.txt";
-	
-		FileOutputStream strumieñWyjœciowy= new FileOutputStream(plik);
-		strumieñWyjœciowy.close();   
-		  
-        } catch(FileNotFoundException e){
-            System.out.println("Nie znaleziono pliku");
-        } catch(IOException e){
+		
+			 strumien = new RandomAccessFile(plik, "rw");
+		
+		         } 
+		catch(FileNotFoundException e){
+          
+		        	System.out.println("Nie znaleziono pliku");}
+		
+		        	try{
+		        		strumien.writeUTF("Danusia");
+		        		strumien.writeUTF("2017");
+		        		strumien.writeDouble(50/4);
+		        		strumien = new RandomAccessFile(plik, "rw");
+		        		
+		        	while (strumien.read() != -1)  
+		        	   iloscSczytanychBajtow ++;
+		        	  }
+         catch(IOException e){
             System.out.println("B³¹d wejœcia-wyjœcia");
         }
+		        	try{ strumien.close();
+		        	
+		        	}catch (IOException e)
+		        	{
+		        	System.out.println("B³¹d zamykania strumienia");
+		        	}
+		        	System.out.println("Iloœæ sczytanych bajtów "+ iloscSczytanychBajtow );
 	}
 
 }
